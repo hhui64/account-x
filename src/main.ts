a4615252c9b879e17cd6e5e6ebabb1984a5b617c
hhui64 <907322015@qq.com>
2020-02-27 10:34:14 +0800

    feat: 将 express 实例传入工厂函数

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
+import * as express from 'express'
+import { NestExpressApplication, ExpressAdapter } from '@nestjs/platform-express'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ConfigService } from './modules/config/config.service'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const server = express()
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, new ExpressAdapter(server))
   await app.listen(app.get(ConfigService).get('APP_PORT'))
 }
 
