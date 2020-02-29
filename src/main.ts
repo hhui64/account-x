497921c10c2099083b0a2cb2d9fcb0a468b0eb3c
hhui64 <907322015@qq.com>
2020-02-29 20:17:42 +0800

    feat:  添加 ValidationPipe 验证管道

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import { NestExpressApplication, ExpressAdapter } from '@nestjs/platform-express
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ConfigService } from './modules/config/config.service'
+import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
   const server = express()
   const app = await NestFactory.create<NestExpressApplication>(AppModule, new ExpressAdapter(server))
+  app.useGlobalPipes(new ValidationPipe())
   await app.listen(app.get(ConfigService).get('APP_PORT'))
 }
 
